e83ec317a5f74948edcc1c1a358aabb815b80d75
Sai Kranthi <iamsaikranthi@gmail.com>
2023-03-20 02:58:39 +0530

    feat: support creating and writing to a file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use color_eyre::eyre::Result;
+use std::path::PathBuf;
+
+use color_eyre::eyre::{eyre, Result, WrapErr};
 use digital_garden::write::write;
+use directories::UserDirs;
 use structopt::StructOpt;
 
 /// A CLI for growing and curation of a digital garden
@@ -8,6 +11,9 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "garden")]
 struct Opt {
+    #[structopt(parse(from_os_str), short = "p", long, env)]
+    garden_path: Option<PathBuf>,
+
     #[structopt(subcommand)]
     cmd: Command,
 }
@@ -26,10 +32,21 @@ enum Command {
     },
 }
 
+fn get_default_garden_dir() -> Result<PathBuf> {
+    let user_dir = UserDirs::new().ok_or_else(|| eyre!("Could not find home directory"))?;
+
+    Ok(user_dir.home_dir().join(".garden"))
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
     let opt = Opt::from_args();
     dbg!(&opt);
+
+    let garden_path = match opt.garden_path {
+        Some(pathbuf) => Ok(pathbuf),
+        None => get_default_garden_dir().wrap_err("garden_path was not supplied"),
+    }?;
     match opt.cmd {
         Command::Write { title } => write(title),
     }
--- src/write.rs
+++ src/write.rs
@@ -1,5 +1,25 @@
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{Result, WrapErr};
+use edit::{edit_file, Builder};
+use std::io::{Read, Write};
+use std::path::PathBuf;
 
-pub fn write(_title: Option<String>) -> Result<()> {
+const TEMPLATE: &[u8; 2] = b"# ";
+
+pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
+    let (mut file, filepath) = Builder::new()
+        .suffix(".md")
+        .rand_bytes(5)
+        .tempfile_in(&garden_path)
+        .wrap_err("Failed to create wip file")?
+        .keep()
+        .wrap_err("Failed to keep tempfile")?;
+    file.write_all(TEMPLATE);
+    // let the user write whatever they want in their favorite editor
+    // before returning to the cli and finishing up
+    edit_file(filepath)?;
+    // Read the user's changes back from the file into a string
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
     todo!()
 }
