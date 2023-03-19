05309f35ffd6463d4e7e047e77484d2b71814ff2
Sai Kranthi <iamsaikranthi@gmail.com>
2023-03-19 23:27:38 +0530

    feat: add documentation for write subcommand and title

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use color_eyre::eyre::Result;
 use structopt::StructOpt;
 
+/// A CLI for growing and curation of a digital garden
+/// 
+/// Visit https://github.com/kranthicodes/digital-garden-cli for more
 #[derive(StructOpt, Debug)]
+#[structopt(name = "garden")]
 struct Opt {
     #[structopt(subcommand)]
     cmd: Command,
@@ -9,7 +13,13 @@ struct Opt {
 
 #[derive(StructOpt, Debug)]
 enum Command {
+    /// write something in your garden
+    /// 
+    /// This command will open your $EDITOR, wair for you
+    /// to write something, and then saves the file to your
+    /// garden
     Write {
+        /// Optionally set a title for what you are going to write about
         #[structopt(short, long)]
         title: Option<String>,
     },
